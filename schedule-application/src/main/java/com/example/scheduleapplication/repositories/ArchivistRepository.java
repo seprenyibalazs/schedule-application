95be5a6b68b8d5dc9e851be3ba2c94de669e2c3e
seprenyibalazs <80784538+seprenyibalazs@users.noreply.github.com>
2024-04-29 21:31:26 +0200

    Created a isArchivistWorkThatDay() method that filter the archivist about the assignmentDay.

--- schedule-application/src/main/java/com/example/scheduleapplication/repositories/ArchivistRepository.java
+++ schedule-application/src/main/java/com/example/scheduleapplication/repositories/ArchivistRepository.java
@@ -1,9 +1,17 @@
 package com.example.scheduleapplication.repositories;
 
 import com.example.scheduleapplication.models.Archivist;
+import com.example.scheduleapplication.models.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface ArchivistRepository extends JpaRepository<Archivist, Long> {
+    @Query("SELECT s FROM Schedule s WHERE s.archivist = :archivist AND s.assignmentDay = :assignmentDay")
+    Optional<Schedule> isArchivistWorkThatDay(@Param("archivist") Archivist archivist, @Param("assignmentDay") LocalDate assignmentDay);
 }
