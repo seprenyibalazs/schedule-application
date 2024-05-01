f257d109b1d6b3e4e2efaf527ce282c18f93108e
seprenyibalazs <80784538+seprenyibalazs@users.noreply.github.com>
2024-05-01 19:18:00 +0200

    Created a findNoMatchAssignmentDay() method where filtering the Archivists about the assignmentDay.

--- schedule-application/src/main/java/com/example/scheduleapplication/repositories/ArchivistRepository.java
+++ schedule-application/src/main/java/com/example/scheduleapplication/repositories/ArchivistRepository.java
@@ -8,10 +8,14 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ArchivistRepository extends JpaRepository<Archivist, Long> {
     @Query("SELECT s FROM Schedule s WHERE s.archivist = :archivist AND s.assignmentDay = :assignmentDay")
     Optional<Schedule> isArchivistWorkThatDay(@Param("archivist") Archivist archivist, @Param("assignmentDay") LocalDate assignmentDay);
+
+    @Query("SELECT DISTINCT a FROM Archivist a WHERE NOT EXISTS (SELECT s FROM Schedule s WHERE s.archivist = a AND (s.assignmentDay = :assignmentDay OR s.assignmentDay IS NULL))")
+    List<Archivist> findNoMatchAssignmentDay(@Param("assignmentDay") LocalDate assignmentDay);
 }
